a54d3e3aae98e7cf1fb4b140c8d077b27f171ae6
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-07-04 18:15:26 +0300

    0001

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Sources auto-generation and formatting
+SPBSUT
+
+Saganenko A.V
+Kuxin A.A
+Maximenko S.O
+Markin N.
+Scherbakova N.
+Ibragimova A.
+
+main.py
+PyCharm 2022.1.3
+Python 3.9
+"""
+
+
+def main():
+    print("log")
+
+
+if __name__ == "__main__":
+    main()
