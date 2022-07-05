a10705eda62baffb7198e0160e0a7e08eb94fcd1
SaharoK-Er404 <lool0308@mail.ru>
2022-07-05 19:24:48 +0300

    0002

--- backend/format.py
+++ backend/format.py
@@ -27,4 +27,20 @@ def gostformat(resource_type, resource_stack):
             result += " " + resource_stack[1]
             result += ". – " + resource_stack[2] + ", " + resource_stack[3] + ". - "
             result += resource_stack[4] + " с."
+    elif resource_type == "article":
+        if len(resource_stack) != 0:
+            result += resource_stack[0]
+            result += " " + resource_stack[1]
+            result += " // " + resource_stack[2] + ". - " + resource_stack[3] + ". - "
+            result += resource_stack[4] + ". - С. " + resource_stack[5] + "."
+    elif resource_type == "webresource":
+        if len(resource_stack) != 0:
+            result += resource_stack[0]
+            result += " " + resource_stack[1] + " [Электронный ресурс]. URL:"
+            result += resource_stack[2] + " (дата обращения: " + resource_stack[3] + ")"
+
+## ("book", ("Glinka A.A.", "Lovers and Dungeons", "DolbitNorm", "2020", "431"))
+## ("article", ("Glinka A.A.", "Lovers and Dungeons", "DolbitNorm", "2020", "№228" "100-121"))
+## ("webresource", ("Glinka A.A.", "Lovers and Dungeons", "http://DolbitNorm", "05.05.2020""))
+
     return result
