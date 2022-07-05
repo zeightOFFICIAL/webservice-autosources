06ccf4c1a48cf8e8e2958f5d9adfa2dafaf19f4f
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-07-05 19:27:36 +0300

    0003

--- djsite/djsite/views.py
+++ djsite/djsite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    return render(request, 'index.html')
