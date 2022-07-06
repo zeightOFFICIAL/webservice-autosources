a75acb919ad2a9c03cdda2567b695fcdbebb544e
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-07-06 18:30:24 +0300

    0010

--- djsite/views.py
+++ djsite/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from meat.format import gostformat
 import json
 
@@ -9,4 +8,6 @@ def index(request):
     if request.method == "POST":
         res=gostformat("book", (request.POST.get('surname'), request.POST.get('book-name'), request.POST.get('publish-name'),
                                 request.POST.get('publish-year'), request.POST.get('page-number')))
-    return render(request, 'index.html', {"result":res})
+        return render(request, 'index.html', {"result":res, "main_page_active":"", "user_input_active":"section-visible",
+                                              "main_button_active":"", "user_button_active":"active"})
+    return render(request, 'index.html', {"main_page_active":"section-visible", "main_button_active":"active"})
