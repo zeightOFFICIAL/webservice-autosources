c9fc67f90979ea45ec6f18dda4598142bcfd871a
Artemii <50341618+zeightOFFICIAL@users.noreply.github.com>
2022-07-07 16:05:43 +0300

    0013

--- djsite/views.py
+++ djsite/views.py
@@ -5,18 +5,29 @@ from meat.format import gostformat
 def index(request):
     res=""
     if request.method == "POST":
-        print("u")
         if request.POST.get("type") == "type_book":
-            print('s')
             res=gostformat("book", (request.POST.get('surname'), request.POST.get('book-name'), request.POST.get('publish-name'),
                                     request.POST.get('publish-year'), request.POST.get('page-number')))
-            return render(request, 'index.html', {"result":res, "main_page_active":"", "user_input_active":"section-visible",
-                                              "main_button_active":"", "user_button_active":"active"})
+            return render(request, 'index.html', {
+                                                 "result":res, "result2":"", "result3":"", 
+                                                 "main_page_active":"", "user_input_active":"section-visible",
+                                                 "main_button_active":"", "user_button_active":"active"
+                                                 })
         elif request.POST.get("type") == "type_article":
             res=gostformat("article", (request.POST.get('author-surname'), request.POST.get('article-name'), request.POST.get('magazine-name'),
                                     request.POST.get('article-publish-year'), request.POST.get('magazine-number'), 
                                     request.POST.get('magazine-article-number')))
-            return render(request, 'index.html', {"result":res, "main_page_active":"", "user_input_active":"section-visible",
-                                              "main_button_active":"", "user_button_active":"active"})
-
+            return render(request, 'index.html', {
+                                                 "result":"", "result2":res, "result3":"",
+                                                 "main_page_active":"", "user_input_active":"section-visible",
+                                                 "main_button_active":"", "user_button_active":"active",
+                                                 })
+        elif request.POST.get("type") == "type_source":
+            res=gostformat("webresource", (request.POST.get('resource-author'), request.POST.get('resource-name'), request.POST.get('resource-link'),
+                                           request.POST.get('visit-date')))
+            return render(request, "index.html", {
+                                                 "result":"", "result2":"", "result3":res,
+                                                 "main_page_active":"", "user_input_active":"section-visible",
+                                                 "main_button_active":"", "user_button_active":"active",
+                                                 })
     return render(request, 'index.html', {"main_page_active":"section-visible", "main_button_active":"active"})
--- meat/format.py
+++ meat/format.py
@@ -1,24 +1,3 @@
-"""
-format.py
-PyCharm 2022.1.3
-Python 3.9
-"""
-"""
-type =
-book or article or web-source
-source stack = 
-{
-author and initials,    -
-title,                  -
-issue number,           -
-city of issue,          -
-name of publisher,      -
-date of issue,          -
-amount of pages         -
-}
-"""
-
-
 def gostformat(resource_type, resource_stack):
     result = ""
     if resource_type == "book":
@@ -42,5 +21,4 @@ def gostformat(resource_type, resource_stack):
 ## ("book", ("Glinka A.A.", "Lovers and Dungeons", "DolbitNorm", "2020", "431"))
 ## ("article", ("Glinka A.A.", "Lovers and Dungeons", "DolbitNorm", "2020", "№228" "100-121"))
 ## ("webresource", ("Glinka A.A.", "Lovers and Dungeons", "http://DolbitNorm", "05.05.2020""))
-
     return result
